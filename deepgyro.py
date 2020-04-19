1a4faf964d1eb79a589a1e74722ea1215b10d194
Agchai52 <shuangzhang0809@gmail.com>
2020-04-19 17:26:16 -0400

    test all for new AidedDeblur

--- deepgyro.py
+++ deepgyro.py
@@ -32,8 +32,8 @@ blury_names = os.listdir(blury_path)
 
 blurred_names = sorted(blurred_names)
 for fname in blurred_names:
-    if fname[:6] <= '033751':
-        continue
+    #if fname[:6] <= '033751':
+    #    continue
     keras.clear_session()
         
     print("Deblurring '%s' with DeepGyro" %(fname))
